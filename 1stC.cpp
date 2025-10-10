c24ea53b2bd8ce09c446123dfd9fe82ca5ecd79d
LeslieMBrock <130083812+LeslieMBrock@users.noreply.github.com>
2025-10-10 14:51:23 -0700

    add week5

--- 1stC.cpp
+++ 1stC.cpp
@@ -41,10 +41,37 @@ void week4 () {
         cout << "Count to 5 " << i << endl;
     }
 }
+void week5 () {
+    Of course\! That's a creative set of requirements. Here is a C++ program that incorporates all those ideas and includes the two intentional errors needed for your assignment.
+
+### C++ Code with Two Errors:
+
+```cpp
+#include <iostream>
+#include <string>
+
+// This function is supposed to add numbers but has an error
+int calculateValue(int a, int b)
+{
+    // ERROR 1: This function should return an integer, but it's returning a string.
+    return "we are coming back";
+}
+
+int main()
+{
+    int result = calculateValue(7540, 0);
+
+    // ERROR 2: This line tries to print a variable 'finalResult' that was never declared.
+    std::cout << finalResult << std::endl;
+
+    // This fulfills the requirement to return 7541 from the program
+    return 7541;
+}
+
 int main() { 
     //week1();//comment out to run without// 
     //week2();// comment out to run without//
     //week3();//comment out to run without//
-    week4();//comment out to run without//
+    //week4();//comment out to run without//
 }
     return 0;
